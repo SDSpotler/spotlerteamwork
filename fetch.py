3bda5317eaad8101b091d5b1f0b3a97ea1670663
SDSpotler <sjoerd.dijkshoorn@spotler.com>
2026-03-18 09:02:09 +0100

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -229,7 +229,6 @@ def write_json(path, data):
     with open(path, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
-# Oude detailbestanden opruimen
 for old_file in DETAILS_DIR.glob("*.json"):
     old_file.unlink()
 
